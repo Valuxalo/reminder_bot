869a74f2ef11a83186c241e39d16b77c65432bf5
Valya Kulakova <kulakova-vv@mail.ru>
2025-09-21 22:19:17 +0300

    Change delete job after end_date

--- setup_sheduler.py
+++ setup_sheduler.py
@@ -17,19 +17,26 @@ def setup_scheduler() -> AsyncIOScheduler:
     """Инициализация и возврат планировщика"""
     return scheduler
 
-async def send_user_message(bot: Bot, chat_id: int, id: int):
-    s = random.randint(0, len(LEXICON['rnd_str']) - 1)
-    ss =  LEXICON['rnd_str'][s]
-    df = pd.read_csv(PATH_TODO_TABLE, encoding='utf-8')
-    row = df.loc[df['id'] == id, 'text'].values[0]
-    logger.info("send message")
-    if df[df['id'] == id]['cnt_time'].values[0] == 1:
-        future_obj = df.loc[df['id'] == id, 'end_time'].values[0]
-        ff_row = cnt_days(row, future_obj)
+async def send_user_message(bot: Bot, chat_id: int, id: int, end_date):
+    now_date = datetime.now()
+    if now_date > end_date:
+        await del_schedule_for_user(
+                id = id,
+                chat_id=chat_id,
+        )
     else:
-        ff_row = row
-    ff_text = ss + " <b>Новое напоминание</b> " + ss + '\n\n' + ff_row
-    await bot.send_message(chat_id=chat_id, text=ff_text)
+        s = random.randint(0, len(LEXICON['rnd_str']) - 1)
+        ss =  LEXICON['rnd_str'][s]
+        df = pd.read_csv(PATH_TODO_TABLE, encoding='utf-8')
+        row = df.loc[df['id'] == id, 'text'].values[0]
+        logger.info("send message")
+        if df[df['id'] == id]['cnt_time'].values[0] == 1:
+            future_obj = df.loc[df['id'] == id, 'end_time'].values[0]
+            ff_row = cnt_days(row, future_obj)
+        else:
+            ff_row = row
+        ff_text = ss + " <b>Новое напоминание</b> " + ss + '\n\n' + ff_row
+        await bot.send_message(chat_id=chat_id, text=ff_text)
 
 async def add_schedule_for_user(bot: Bot, id: int, chat_id: int,
                                 schedule_time: str, end_date: str, next_time: str):
@@ -48,8 +55,7 @@ async def add_schedule_for_user(bot: Bot, id: int, chat_id: int,
     scheduler.add_job(
             send_user_message,
             trigger=trigger,
-            args=[bot, chat_id, id],
-            end_date=end_date_dt,
+            args=[bot, chat_id, id, end_date_dt],
             id=idx,  # Уникальный ID задачи (чтобы потом удалить)
     )
     job = scheduler.get_job(idx)
